b59d6ff610cb0a188a261e496a08aceb82d6c9c7
Asper Hsu <asperwon@gmail.com>
2016-08-24 23:32:25 +0800

    add google geocoder to get address

--- src/js/list-page.js
+++ src/js/list-page.js
@@ -12,6 +12,8 @@ moment.updateLocale('zh-tw', {
 	}
 });
 
+
+
 $(function(){
 	// siteName hashtag
 	var siteName = window.location.hash.substr(1);	
@@ -29,5 +31,27 @@ $(function(){
 		} else {
 			$(this).bootstrapTable('expandRow', $tr.data('index'));
 		}
+	});
+
+	google.maps.event.addDomListener(window, "load", function(){
+		var geocoder = new google.maps.Geocoder();
+		var getAddr = function(lat, lng, cb){
+			var coord = new google.maps.LatLng(lat, lng);
+			geocoder.geocode({'latLng': coord }, function(results, status) {
+				if (status === google.maps.GeocoderStatus.OK && results) {					
+					cb(results[0].formatted_address);
+					return;
+				}
+				cb('');
+			});
+		}
+
+		$("body").on("mouseover", "a.latlng", function(){
+			var $el = $(this);
+			var [lat, lng] = $(this).data('latlng').split(',');
+			getAddr(lat, lng, function(addr){
+				$el.attr('title', addr + ` (${$el.data('latlng')})` );
+			});			
+		});
 	});
 });
